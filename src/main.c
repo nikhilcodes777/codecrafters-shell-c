80530490bd50246226eb77e4dcf6075e1d7744eb
Nikhil <nikhilv7x7@gmail.com>
2025-11-05 10:44:57 +0530

    feat: ✨ repl

--- src/main.c
+++ src/main.c
@@ -5,11 +5,11 @@ int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // TODO: Uncomment the code below to pass the first stage
-  printf("$ ");
-  char buf[100];
-  scanf("%s",buf);
-  printf("%s: command not found",buf);
-
+  while (1) {
+    printf("$ ");
+    char buf[100];
+    scanf("%s", buf);
+    printf("%s: command not found\n", buf);
+  }
   return 0;
 }
