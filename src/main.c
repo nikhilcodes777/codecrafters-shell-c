8f140782d8632dbd579bda724698d551b3423dec
Nikhil <nikhilv7x7@gmail.com>
2025-11-05 11:09:20 +0530

    feat: ✨ exit command

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
@@ -8,8 +9,21 @@ int main(int argc, char *argv[]) {
   while (1) {
     printf("$ ");
     char buf[100];
-    scanf("%s", buf);
+    if (!fgets(buf, sizeof(buf), stdin)) {
+      printf("Error reading from stdin\n");
+      exit(0);
+    }
+    int exit_code = 0;
+    if (sscanf(buf, "exit %d", &exit_code) >= 1) {
+      exit(exit_code);
+    }
+    if (strncmp(buf, "exit", 4) == 0) {
+      exit(0);
+    }
+    buf[strcspn(buf, "\n")] = 0;
+
     printf("%s: command not found\n", buf);
   }
+
   return 0;
 }
